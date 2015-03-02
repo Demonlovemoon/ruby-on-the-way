14ab96b6d934d31521581684c30fe91b1603734b
Demonlovemoon <xu.yuexi@microwise-system.com>
2015-03-02 17:10:40 +0800

    learn ruby

--- playground/hash_util.rb
+++ playground/hash_util.rb
@@ -2,12 +2,18 @@ class HashUtil
 
   # 返回 value 是最大数对应的 key
   def key_of_max(hash)
-    # TODO
+    max= hash.values.max+""
+    hash.invert
+    return hash[max]
   end
 
   # 将 key 和 value 组成新的 hash
   def to_hash(key_array, value_array)
-    # TODO
+    h=Hash.new
+    key_array.each_with_index do |elem, i|
+      h[elem]=value_array[i]
+    end
+    return h
   end
 
 end
--- playground/math_util.rb
+++ playground/math_util.rb
@@ -2,21 +2,27 @@ class MathUtil
 
   # 计算两个数的合
   def add(a, b)
-    # TODO
+    a+b
   end
 
   # 返回数组中最大的数
   def max(array)
-    # TODO
+    array.max
   end
 
   # 返回数据中的所有数的合
   def sum(array)
-    # TODO
+    sum1=0
+    array.each do |elem|
+      sum1=sum1+elem
+    end
+    return sum1
   end
 
   # 将数据每个元素成2返回
   def double(array)
-    # TODO
+    array.map do |n|
+      n*2
+    end
   end
 end
